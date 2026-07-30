55c8ebdf2ad07d7a50c08bd191c98df01e9c1737
amittomar12005 <amittomar12005@gmail.com>
2026-07-30 23:54:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ document.getElementById("message").style.display = "none";
 document.getElementById("letter").style.display = "block";
 
 }
+setInterval(createHeart, 300);
+
+function createHeart() {
+
+const heart = document.createElement("div");
+
+heart.className = "heart";
+
+heart.innerHTML = "❤️";
+
+heart.style.left = Math.random() * 100 + "vw";
+
+heart.style.fontSize = (20 + Math.random() * 25) + "px";
+
+document.getElementById("hearts").appendChild(heart);
+
+setTimeout(() => {
+heart.remove();
+}, 6000);
+
+}
