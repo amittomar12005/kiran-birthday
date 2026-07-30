0a2a5d3e14e903650afec5f11f9a45dd02e47fdc
amittomar12005 <amittomar12005@gmail.com>
2026-07-31 00:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,44 @@ heart.remove();
 }, 6000);
 
 }
+const photos = [
+"photo1.jpg",
+"photo2.jpg",
+"photo3.jpg",
+"photo4.jpg",
+"photo5.jpg",
+"photo6.jpg",
+"photo7.jpg",
+"photo8.jpg",
+"photo9.jpg"
+];
+
+let currentPhoto = 0;
+
+function startSlideshow(){
+
+document.getElementById("slideshow").style.display = "block";
+
+setInterval(function(){
+
+currentPhoto++;
+
+if(currentPhoto >= photos.length){
+currentPhoto = 0;
+}
+
+document.getElementById("slide").src = photos[currentPhoto];
+
+},2500);
+
+}
+
+const oldOpenGift = openGift;
+
+openGift = function(){
+
+oldOpenGift();
+
+startSlideshow();
+
+};
