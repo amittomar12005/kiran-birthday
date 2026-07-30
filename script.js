ec76be4fbf1f7e1c246ffa76b9dc88e43b474cef
amittomar12005 <amittomar12005@gmail.com>
2026-07-30 23:16:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function openGift() {
-    alert("Welcome Kiran ❤️\nYour special birthday surprise is loading...");
+function openGift(){
+
+document.getElementById("giftBox").style.display="none";
+
+document.getElementById("message").style.display="block";
+
+document.body.style.background="linear-gradient(135deg,#ff6ec4,#7873f5,#4facfe)";
+
+alert("🎉 Happy Birthday Kiran ❤️");
+
 }
